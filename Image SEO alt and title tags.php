3f0c50f86af48ee6c8a7366e48f3a1073b2eb752
Jason Carney <jason@dashmedia.com.au>
2014-07-25 15:29:05 +0930

    fix to prevent plugin running on non-HTML page output
    Signed-off-by: Jason Carney <jason@dashmedia.com.au>

--- Image SEO alt and title tags.php
+++ Image SEO alt and title tags.php
@@ -1,74 +1,77 @@
 <?php
-$containerConfig = $modx->getOption('img_seo_containers');
-$containerConfig = $containerConfig ? $containerConfig : 'article, section, header, footer, aside';
 
-$headerConfig = $modx->getOption('img_seo_headings');
-$headerConfig = $headerConfig ? $headerConfig : 'h1,h2,h3,h4,h5,h6';
+if($modx->resource->get('content_type') == 1){ //only run on HTML doc types
+	$containerConfig = $modx->getOption('img_seo_containers');
+	$containerConfig = $containerConfig ? $containerConfig : 'article, section, header, footer, aside';
 
-$containers = explode($containerConfig, ',');
-$headers = explode($headerConfig, ',');
-$output = &$modx->resource->_output; // get a reference to the output
-$dom = new DOMDocument;
-@$dom->loadHTML($output);
+	$headerConfig = $modx->getOption('img_seo_headings');
+	$headerConfig = $headerConfig ? $headerConfig : 'h1,h2,h3,h4,h5,h6';
 
-function getParentNode($node, $types){
-	$parent = $node->parentNode;
-	if(in_array($parent->tagName, $types)){
-		return $parent;
-	} else {
-		if($parent->tagName == 'body'){
-			return false;
+	$containers = explode($containerConfig, ',');
+	$headers = explode($headerConfig, ',');
+	$output = &$modx->resource->_output; // get a reference to the output
+	$dom = new DOMDocument;
+	@$dom->loadHTML($output);
+
+	function getParentNode($node, $types){
+		$parent = $node->parentNode;
+		if(in_array($parent->tagName, $types)){
+			return $parent;
 		} else {
-			return getParentNode($parent, $types);
+			if($parent->tagName == 'body'){
+				return false;
+			} else {
+				return getParentNode($parent, $types);
+			}
 		}
 	}
-}
 
-function getHeadingNode($parent, $headers, $containers){
-	$search = true;
-	$index = 0;
-	for($index = 0; $index < count($headers); $index++){
-		$tmp = $parent->getElementsByTagName($headers[$index]);
-		if($tmp->length > 0){
-			//we have headings, return the first
-			return $tmp->item(0);
+	function getHeadingNode($parent, $headers, $containers){
+		$search = true;
+		$index = 0;
+		for($index = 0; $index < count($headers); $index++){
+			$tmp = $parent->getElementsByTagName($headers[$index]);
+			if($tmp->length > 0){
+				//we have headings, return the first
+				return $tmp->item(0);
+			}
+		}
+		$nextParent = getParentNode($parent, $containers);
+		if($nextParent){
+			return getHeadingNode($nextParent, $headers, $containers);
+		} else {
+			// no more parents
+			return false;
 		}
 	}
-	$nextParent = getParentNode($parent, $containers);
-	if($nextParent){
-		return getHeadingNode($nextParent, $headers, $containers);
-	} else {
-		// no more parents
-		return false;
-	}
-}
 
-function setAltText($node, $headers, $containers, $modx){
-	$heading = getHeadingNode($node->parentNode, $headers, $containers);
-	if($heading){
-		//we've found a heading, use it's value as image alt
-		$node->setAttribute('alt', $heading->nodeValue);
-	} else {
-		//use pagetitle
-		$alt = $modx->resource->get('pagetitle');
-		$longtitle = $modx->resource->get('longtitle');
-		if($longtitle){
-			$alt = $longtitle;
+	function setAltText($node, $headers, $containers, $modx){
+		$heading = getHeadingNode($node->parentNode, $headers, $containers);
+		if($heading){
+			//we've found a heading, use it's value as image alt
+			$node->setAttribute('alt', $heading->nodeValue);
+		} else {
+			//use pagetitle
+			$alt = $modx->resource->get('pagetitle');
+			$longtitle = $modx->resource->get('longtitle');
+			if($longtitle){
+				$alt = $longtitle;
+			}
+			$node->setAttribute('alt', $alt);
 		}
-		$node->setAttribute('alt', $alt);
 	}
-}
 
-foreach ($dom->getElementsByTagName('img') as $node) {
-	//find parents matching container array
-	if(!$node->getAttribute('alt')){
-		// we do not have alt text
-		setAltText($node, $headers, $containers, $modx);
+	foreach ($dom->getElementsByTagName('img') as $node) {
+		//find parents matching container array
+		if(!$node->getAttribute('alt')){
+			// we do not have alt text
+			setAltText($node, $headers, $containers, $modx);
+		}
+		if(!$node->getAttribute('title')){
+			//we do not have title text, set it to the same as the alt text
+			$node->setAttribute('title', $node->getAttribute('alt'));
+		}
 	}
-	if(!$node->getAttribute('title')){
-		//we do not have title text, set it to the same as the alt text
-		$node->setAttribute('title', $node->getAttribute('alt'));
-	}
-}
 
-$output = $dom->saveHTML();
+	$output = $dom->saveHTML();
+}
